774d4d6b64b98f0e7e83aee337ef5e2328cd9e33
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 13:44:47 +0500

    Queue at the store

---
+++ QueueStore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueStore
+{
+	internal class QueueStore
+	{
+		static void Main(string[] args)
+		{
+			Queue<int> queueStore = new Queue<int>();
+			int cashAccount = 0;
+
+			FillQueue(queueStore);
+			ClearQueue(queueStore, cashAccount);
+		}
+
+		static void FillQueue(Queue<int> queueStore)
+		{
+			Random random = new Random();
+			int maxValue = 10000;
+
+			for (int i = 0; i < 10; i++)
+				queueStore.Enqueue(random.Next(maxValue + 1));
+		}
+
+		static void ClearQueue(Queue<int> queueStore, int cashAccount)
+		{
+			while (queueStore.Count > 0)
+			{
+				cashAccount += queueStore.Dequeue();
+				Console.WriteLine($"Счёт после покупки: {cashAccount}");
+
+				Console.WriteLine("Нажмите любую клавишу, чтобы продолжить");
+				Console.ReadKey();
+			}
+			Console.WriteLine("Больше покупателей нет");
+		}
+	}
+}
